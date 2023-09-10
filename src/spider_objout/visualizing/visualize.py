c20b2d6659c51d84d8c223757c80ab9a86db2e4c
objout <android-ios@qq.com>
2023-09-10 16:37:34 +0800

    Add the visualizing layer.

---
+++ src/spider_objout/visualizing/visualize.py
@@ -0,0 +1,64 @@
+import os
+
+import jieba
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+from ..persistent.persistent import Persistent
+
+
+class Visualize:
+    def __init__(self, filename='persis.xlsx'):
+        """Read or write the workbook specified by filename
+
+        :param filename: the file to manipulate
+        :type filename: string
+        """
+        self.filename = filename
+
+        self.cdir = os.path.dirname(__file__)
+        self.font_path = f'{self.cdir}/simsun.ttf'
+        self.stopwords_path = f'{self.cdir}/stopwords.txt'
+
+        if not os.path.exists(self.font_path):
+            return
+        if not os.path.exists(self.stopwords_path):
+            return
+
+        self.stopwords = set()
+        self.stopwords.update([
+            line.strip() for line in
+            open(self.stopwords_path, 'r').readlines()
+        ])
+        self.comment_list = []
+        self.text = ''
+
+        self.per = Persistent(self.filename)
+
+    def readcomments(self):
+        """Get the comment list from the specified filename."""
+        self.comment_list = self.per.read()
+
+    def cutword(self):
+        """Cut the sentence in the comment list.
+        The result is delimited by space.
+        """
+        for item in self.comment_list:
+            self.text += ' '.join(jieba.cut(item))
+
+    def render(self):
+        """Visualizing the result."""
+        wordcloud = WordCloud(
+            font_path=self.font_path,
+            width=700,
+            height=700,
+            background_color='white',
+            stopwords=self.stopwords).generate(self.text)
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis('off')
+        plt.show()
+
+    def run(self):
+        self.readcomments()
+        self.cutword()
+        self.render()
