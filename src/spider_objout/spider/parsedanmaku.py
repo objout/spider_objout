fae6964142390c8f36ff8d9694cf2b8fa546879a
objout <android-ios@qq.com>
2023-09-10 15:12:27 +0800

    Add the parsedanmaku module.

---
+++ src/spider_objout/spider/parsedanmaku.py
@@ -0,0 +1,22 @@
+import re
+
+from google.protobuf import text_format
+
+from .utils import bili_pb2
+
+
+class ParseDanmaku:
+    def __init__(self):
+        self.reg = '.*?content:.*?"(.*?)".*?ctime.*?'
+        self.pattern = re.compile(self.reg, re.S)
+
+    def pasrse(self, data=''):
+        """Parse the protobuf data."""
+        danmakulist = []
+        my_seg = bili_pb2.DmSegMobileReply()  # type: ignore
+        my_seg.ParseFromString(data)
+        for j in my_seg.elems:
+            res = text_format.MessageToString(j, as_utf8=True)
+            content = self.pattern.match(res).group(1)  # type: ignore
+            danmakulist.append(content)
+        return danmakulist
