fa229c59d95a87480c97b8bcff1253f9e5059a85
objout <android-ios@qq.com>
2023-09-10 15:09:10 +0800

    Add the fetchdanmaku module.

---
+++ src/spider_objout/spider/fetchdanmaku.py
@@ -0,0 +1,25 @@
+from .utils.fetch import Fetch
+
+
+class FetchDanmaku:
+    def __init__(self):
+        self.fet = Fetch()
+
+        self.url = 'https://api.bilibili.com/x/v2/dm/wbi/web/seg.so'
+        self.cid = '1196257131'
+        self.params = {
+            'type': '1',
+            'oid': self.cid,
+            'segment_index': '1',
+            'pull_mode': '1',
+            'ps': '0',
+            'pe': '120000',
+        }
+
+    def fetchdanmaku(self, cid=''):
+        if cid == '':
+            return
+        self.params["oid"] = cid
+        req = self.fet.reqwrapper(self.url, self.params)
+        resp = self.fet.request(req)
+        return resp
