83bcd1538d2c0bd264ec3870e91f85f305428385
objout <android-ios@qq.com>
2023-09-14 12:22:19 +0800

    Add multithreading support.

--- src/spider_objout/main.py
+++ src/spider_objout/main.py
@@ -1,3 +1,4 @@
+import threading
 from .persistent.persistent import Persistent
 from .spider.utils.fetch import Fetch
 from .spider.fetchcid import FetchCid
@@ -6,6 +7,31 @@ from .spider.fetchvid import FetchVideoId
 from .spider.parsedanmaku import ParseDanmaku
 from .visualizing.visualize import Visualize
 
+fet = Fetch()
+fetcid = FetchCid(fet)
+fetdan = FetchDanmaku(fet)
+parsedan = ParseDanmaku()
+fetvid = FetchVideoId(fet)
+
+danmakumap = {}
+
+
+def fetchDanmakuContent(vidl=[]):
+    for vid in vidl:
+        cid = fetcid.fetchcid(vid)
+        raw = fetdan.fetchdanmaku(cid)  # type:ignore
+        res = parsedan.parse(raw)  # type:ignore
+        for item in res:
+            k = item.strip()
+            danmakumap[k] = danmakumap.get(k, 0) + 1  # type:ignore
+
+
+def create_thread(vl):
+    ret = threading.Thread(
+            target=fetchDanmakuContent,
+            args=(vl,))
+    return ret
+
 
 def main():
     try:
@@ -13,29 +39,30 @@ def main():
         kwd = '日本核污染水排海'
         page = 8
 
-        fet = Fetch()
-        fetvid = FetchVideoId(fet)
-        fetcid = FetchCid(fet)
-        fetdan = FetchDanmaku(fet)
-        parsedan = ParseDanmaku()
         per = Persistent(fname)
         vs = Visualize(fname)
 
         vidlist = fetvid.fetchvid(kwd=kwd, page=page)
-        danmakumap = {}
+        p1 = len(vidlist) >> 2
+        p2 = p1 << 1
+        p3 = p1 * 3
 
-        for vid in vidlist:
-            cid = fetcid.fetchcid(vid)
-            raw = fetdan.fetchdanmaku(cid)  # type:ignore
-            res = parsedan.parse(raw)  # type:ignore
-            for item in res:
-                k = item.strip()
-                danmakumap[k] = danmakumap.get(k, 0) + 1
+        threads = []
+        threads.append(create_thread(vidlist[:p1]))
+        threads.append(create_thread(vidlist[p1:p2]))
+        threads.append(create_thread(vidlist[p2:p3]))
+        threads.append(create_thread(vidlist[p3:]))
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
 
+        global danmakumap
         danmakumap = sorted(
             zip(danmakumap.values(), danmakumap.keys()),
             reverse=True
         )
+
         per.write(danmakumap)
         print(danmakumap[:20])
         vs.run()
